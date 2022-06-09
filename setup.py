a964c2b52677ea5172e1d85fa4706e2dcf64f96c
Pep Llaneras <josep.llaneras@proton.ch>
2022-06-09 18:00:28 +0200

    Remove proton-vpn-servers dependency

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@ setup(
     url="https://github.com/ProtonVPN/pyhon-protonvpn-network-manager",
     packages=find_namespace_packages(include=["proton.vpn.backend.linux.networkmanager.core"]),
     include_package_data=True,
-    install_requires=["proton-core", "proton-vpn-connection", "proton-vpn-servers",
-                      "dbus-network-manager", "PyGObject", "dbus-python"],
+    install_requires=["proton-core", "proton-vpn-connection", "dbus-network-manager", "PyGObject", "dbus-python"],
     extras_require={
         "development": ["wheel", "pytest", "pytest-coverage"]
     },
